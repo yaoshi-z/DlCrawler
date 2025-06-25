0c39a8e61d8ece2b51fe76ff3da1237043752937
yaoshi <402203271@qq.com>
2025-06-25 16:38:44 +0800

    完善了json/csv转换工具的逻辑

--- DlCrawler/utils/json_csv_t.py
+++ DlCrawler/utils/json_csv_t.py
@@ -2,8 +2,16 @@ import pandas as pd
 import pathlib
 
 dir_path = pathlib.Path(__file__).parent.parent / "exports"
-file_name = "taobao_search_keywords_音响_20250625151931.json"
-
+file_name = "weibo_search_keywords_扫地机器人_20250616_175642.json"
 file_path = dir_path / file_name
-df = pd.read_json(file_path)
-df.to_csv(dir_path / "taobao_search_keywords_音响_20250625151931.csv", index=False)
+
+if file_name.endswith(".json"):
+    df = pd.read_json(file_path)
+    csv_path = file_path.with_suffix(".csv")
+    df.to_csv(csv_path, index=False)
+    print(f"{file_name} 转换为 {csv_path.name} 成功")
+elif file_name.endswith(".csv"):
+    df = pd.read_csv(file_path)
+    json_path = file_path.with_suffix(".json")
+    df.to_json(json_path, orient="records")
+    print(f"{file_name} 转换为 {json_path.name} 成功")
