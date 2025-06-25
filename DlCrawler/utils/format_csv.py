4501d967e73703ed6fa197ffbb8b296b364cec60
yaoshi <402203271@qq.com>
2025-06-25 17:10:29 +0800

    增加了format_csv脚本工具,解决gbk编码在vscode中打开乱码的问题

---
+++ DlCrawler/utils/format_csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import pathlib
+
+file_name = "模板列表.csv"
+dir_path = pathlib.Path(__file__).parent.parent.parent
+file_path = dir_path / file_name
+
+try:
+    df = pd.read_csv(file_path,encoding="gbk")
+    df.to_csv(file_path, index=False,encoding="utf-8-sig")
+    print(f"{file_name} 文件转换完成,gbk编码转换成utf-8-sig编码")
+except Exception as e:
+    print(f"{file_name} 文件转换失败: {e},源文件非gbk编码")
